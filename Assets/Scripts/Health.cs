ae810d216a245951af3d73a1bcb9f9ac0ec05958
Jenso69 <antheacaruana8@gmail.com>
2025-05-21 17:38:34 +0200

    .

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,46 @@
+using TMPro;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int points = 5;
+    public Vector3 respawnPosition;
+    public TMP_Text healthText;
+    public EndScreenAnimation gameOverScreen;
+
+    private void Start()
+    {
+        respawnPosition = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Trap"))
+        {
+            Damage(1);
+
+        }
+
+        if (other.CompareTag("CheckPoint"))
+        {
+            respawnPosition = other.transform.position;
+            respawnPosition.y = transform.position.y;
+
+        }
+    }
+    private void Damage(int value)
+    {
+        points = points - value;
+        healthText.text = $"<b>Health:</b> {points}";
+
+            transform.position = respawnPosition;
+
+
+        if (points > 1)
+
+            gameOverScreen.StartFade();
+            Destroy(gameObject);
+
+    }
+
+}
---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class LevelExit : MonoBehaviour
+{
+    public EndScreenAnimation winScreen;
+
+    private void OnTriggerEnter(Collider other)
+    { 
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.SetActive(false);
+        winScreen.StartFade();
+        }
+    
+    }
+
+
+}
